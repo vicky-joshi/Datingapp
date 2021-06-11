6c76db2174a59d81128a249a280e95ce2fa8bf3e
vicky-joshi <joshivicky56@gmail.com>
2021-06-11 12:31:06 +0530

    Pending commit for new components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,11 +6,23 @@ import { NavComponent } from './nav/nav.component'
 import{FormsModule} from '@angular/forms'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { MemberDetailsComponent } from './member-details/member-details.component';
+import { ListComponent } from './list/list.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailsComponent,
+    ListComponent,
+    MessagesComponent
   ],
 
   imports: [
